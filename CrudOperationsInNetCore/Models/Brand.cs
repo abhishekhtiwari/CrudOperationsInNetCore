8ef10e27db93362d7afa09383fe7a5693f459563
Abhishek Tiwari <mr.abhishekhtiwari@gmail.com>
2024-06-10 21:59:43 +0530

    Add project files.

---
+++ CrudOperationsInNetCore/Models/Brand.cs
@@ -0,0 +1,10 @@
+﻿namespace CrudOperationsInNetCore.Models
+{
+    public class Brand
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public int IsActive { get; set; }
+    }
+}
---
+++ CrudOperationsInNetCore/Models/BrandContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CrudOperationsInNetCore.Models
+{
+    public class BrandContext:DbContext
+    {
+        public BrandContext(DbContextOptions<BrandContext> options):base(options)
+        {
+
+        }
+        public DbSet<Brand> Brands { get; set; }
+    }
+}
